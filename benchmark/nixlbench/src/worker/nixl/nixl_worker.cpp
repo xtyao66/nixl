3164ead43257994da2d2f804e1d1ae2f550db178
Ofer Achler <oachler@nvidia.com>
2026-06-02 13:15:01 +0300

    Removed duplicate file_offset changes

--- benchmark/nixlbench/src/worker/nixl/nixl_worker.cpp
+++ benchmark/nixlbench/src/worker/nixl/nixl_worker.cpp
@@ -1258,7 +1258,6 @@ xferBenchNixlWorker::exchangeIOV(const std::vector<std::vector<xferBenchIOV>> &l
                         } else {
                             file_offset += block_size;
                         }
-                        file_offset += block_size;
                         fd_idx = 0;
                     }
                 }
